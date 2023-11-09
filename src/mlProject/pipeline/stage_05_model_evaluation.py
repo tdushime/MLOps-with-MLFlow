88bcad945f16fb01c1181570d5563bc0ab8e0535
Theo Phil Dushime <101282559+tdushime@users.noreply.github.com>
2023-11-09 13:32:46 -0500

    App.py created

--- src/mlProject/pipeline/stage_05_model_evaluation.py
+++ src/mlProject/pipeline/stage_05_model_evaluation.py
@@ -14,8 +14,6 @@ class ModelEvaluationTrainingPipeline:
         model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
         model_evaluation_config.log_into_mlflow()
 
-
-
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
